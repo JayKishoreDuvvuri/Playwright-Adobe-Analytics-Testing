f2d2e98d36806c21aefb5fa4e78a0ab7f96d1fc1
Jay Kishore Duvvuri <jaykishore96@ymail.com>
2023-06-26 15:13:38 +0300

    First Commit

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
 	testDir: 'tests',
 	timeout: 30000,
-	retries: 0,
+	retries: 1,
 	reporter: [['junit', { outputFile: 'results.xml' }], ['list']],
 	projects: [
 		{
