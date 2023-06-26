ef517511615f6e5749229386e1834382688e9da6
Jay Kishore Duvvuri <jaykishore96@ymail.com>
2023-06-26 16:01:52 +0300

    First Commit

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ export const baseUrl = 'https://www.xfinity.com/overview'
 export const mobileOverviewPageUrl =
 	'https://www.xfinity.com/learn/mobile-service'
 export const title = 'Mobile Service from Xfinity'
+export const timeout = 5000
